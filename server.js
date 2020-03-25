d857a7677fd951c4eb6318cebcdf6ccbb4a93c81
clueking1 <wcoan300@gmail.com>
2020-03-25 09:38:38 -0400

    registered the service worker and fixed a typo in server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const logger = require("morgan");
+//const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.en.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
