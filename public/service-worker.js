50ff54447a3312bfd5bbbc549a8de51194ed24e6
clueking1 <wcoan300@gmail.com>
2020-03-25 09:59:23 -0400

    added fetch and page still appears if internet goes down

--- public/service-worker.js
+++ public/service-worker.js
@@ -36,6 +36,49 @@ self.addEventListener('activate', event => {
                     })
                 )
             })
-            .then(() => self.ClientRectList.claim())
+            .then(() => self.clients.claim())
     )
 })
+
+self.addEventListener("fetch", event => {
+
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+
+    if (event.request.url.includes("/api/transaction")) {
+
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
+        })
+      );
+      return;
+    }
+
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  });
+
